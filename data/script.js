f1485829ea797d7db3bd084ff51d42cb9df0f8b2
imferr <117535778+imferr@users.noreply.github.com>
2023-11-02 13:22:49 -0400

    creando carpeta data, index, style, script

---
+++ data/script.js
@@ -0,0 +1,12 @@
+function obtenerFechaHora() {
+    const ahora = new Date();
+    const opciones = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false };
+    return ahora.toLocaleDateString('es-ES', opciones);
+}
+
+function actualizarFechaHora() {
+    const fechaHoraElemento = document.getElementById('fecha-hora');
+    if (fechaHoraElemento) {
+        fechaHoraElemento.textContent = obtenerFechaHora();
+    }
+}
